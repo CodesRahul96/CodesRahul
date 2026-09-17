57bdcb1fd90ebeb721bec90bb63e54f5549fe239
Rahul Misal <codesrahul96@gmail.com>
2026-09-17 23:29:23 +0530

    Remove old projects: Netflix Clone, IMDB Clone, Logic Website, AI Chatbot, and Image Gallery

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -3,15 +3,10 @@ import trentshopnow from "./trentshopnow.png";
 import portfolio from "./codesrahul.png";
 import videox from "./video-x.png";
 import gymwala from "./GymWala.png";
-import netflix from "./netflix-clone.png";
-import imdb from "./IMDB.png";
 import livetv from "./livetv-new.jpg";
 import taskmanager from "./task-management.png";
-import imagegallery from "./image-gallary.png";
-import logic from "./logic.png";
 import comingsoon from "./comingsoon.png";
 import desidelights from "./desidelight.png";
-import chatbot from "./chatbot.jpg";
 import exclusivetv from "./ExclusiveTV.png";
 import playlist from "./playlist.png";
 import todolist from "./desktop_todolist.png";
@@ -27,15 +22,10 @@ export {
   portfolio,
   videox,
   gymwala,
-  netflix,
-  imdb,
   livetv,
   taskmanager,
-  imagegallery,
-  logic,
   comingsoon,
   desidelights,
-  chatbot,
   exclusivetv,
   playlist,
   todolist,
