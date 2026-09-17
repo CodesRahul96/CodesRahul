909c3a178f272a44ea438eface52c9887e800801
Rahul Misal <codesrahul96@gmail.com>
2026-09-17 23:38:23 +0530

    Standardize project photo filenames and folder structures

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,37 +1,37 @@
 import blogsify from "./blogsify.png";
-import trentshopnow from "./trentshopnow.png";
-import portfolio from "./codesrahul.png";
-import videox from "./video-x.png";
-import gymwala from "./GymWala.png";
-import livetv from "./livetv-new.jpg";
-import taskmanager from "./task-management.png";
 import comingsoon from "./comingsoon.png";
-import desidelights from "./desidelight.png";
-import playlist from "./playlist.png";
-import todolist from "./desktop_todolist.png";
-import taskflow from "./taskflow.png";
+import desidelights from "./desidelights.png";
+import gymwala from "./gymwala.png";
+import livetv from "./livetv.jpg";
 import omencontrol from "./omencontrol.png";
+import playlist from "./playlist.png";
 import playnox from "./playnox.jpg";
+import portfolio from "./portfolio.png";
+import taskflow from "./taskflow.png";
+import taskmanager from "./taskmanager.png";
+import todolist from "./todolist.png";
+import trentshopnow from "./trentshopnow.png";
 import udemywala from "./udemywala.png";
+import videox from "./videox.png";
 
 // ExclusiveTV Android App Screenshots
-import exclusivetv1 from "./ExclusiveTV/exclusivetv-1.jpg";
-import exclusivetv2 from "./ExclusiveTV/exclusivetv-2.jpg";
-import exclusivetv3 from "./ExclusiveTV/exclusivetv-3.jpg";
-import exclusivetv4 from "./ExclusiveTV/exclusivetv-4.jpg";
-import exclusivetv5 from "./ExclusiveTV/exclusivetv-5.jpg";
-import exclusivetv6 from "./ExclusiveTV/exclusivetv-6.jpg";
-import exclusivetv7 from "./ExclusiveTV/exclusivetv-7.jpg";
-import exclusivetv8 from "./ExclusiveTV/exclusivetv-8.jpg";
-import exclusivetv9 from "./ExclusiveTV/exclusivetv-9.jpg";
-import exclusivetv10 from "./ExclusiveTV/exclusivetv-10.jpg";
-import exclusivetv11 from "./ExclusiveTV/exclusivetv-11.jpg";
+import exclusivetv1 from "./exclusivetv/exclusivetv-01.jpg";
+import exclusivetv2 from "./exclusivetv/exclusivetv-02.jpg";
+import exclusivetv3 from "./exclusivetv/exclusivetv-03.jpg";
+import exclusivetv4 from "./exclusivetv/exclusivetv-04.jpg";
+import exclusivetv5 from "./exclusivetv/exclusivetv-05.jpg";
+import exclusivetv6 from "./exclusivetv/exclusivetv-06.jpg";
+import exclusivetv7 from "./exclusivetv/exclusivetv-07.jpg";
+import exclusivetv8 from "./exclusivetv/exclusivetv-08.jpg";
+import exclusivetv9 from "./exclusivetv/exclusivetv-09.jpg";
+import exclusivetv10 from "./exclusivetv/exclusivetv-10.jpg";
+import exclusivetv11 from "./exclusivetv/exclusivetv-11.jpg";
 
 // StreamPlus Android App Screenshots
-import streamplus1 from "./StreamPlus/streamplus-1.jpg";
-import streamplus2 from "./StreamPlus/streamplus-2.jpg";
-import streamplus3 from "./StreamPlus/streamplus-3.jpg";
-import streamplus4 from "./StreamPlus/streamplus-4.jpg";
+import streamplus1 from "./streamplus/streamplus-01.jpg";
+import streamplus2 from "./streamplus/streamplus-02.jpg";
+import streamplus3 from "./streamplus/streamplus-03.jpg";
+import streamplus4 from "./streamplus/streamplus-04.jpg";
 
 const exclusivetv = [
   exclusivetv1,
@@ -56,20 +56,20 @@ const streamplus = [
 
 export {
   blogsify,
-  trentshopnow,
-  portfolio,
-  videox,
-  gymwala,
-  livetv,
-  taskmanager,
   comingsoon,
   desidelights,
-  playlist,
-  todolist,
-  taskflow,
+  gymwala,
+  livetv,
   omencontrol,
+  playlist,
   playnox,
+  portfolio,
+  taskflow,
+  taskmanager,
+  todolist,
+  trentshopnow,
   udemywala,
+  videox,
   // Multi-image collections
   exclusivetv,
   streamplus,
