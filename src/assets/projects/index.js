6fc29cf29dc551510eae91113e0272779cbf99bd
Rahul Misal <codesrahul96@gmail.com>
2026-09-25 23:21:11 +0530

    fix(assets): replace crunchyroll bare logo with proper desktop app screenshot

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,6 +1,6 @@
 import blogsify from "./blogsify.png";
 import comingsoon from "./comingsoon.png";
-import crunchyroll from "./crunchyroll.png";
+import crunchyroll from "./crunchyroll.jpg";
 import desidelights from "./desidelights.png";
 import gymwala from "./gymwala.png";
 import freelivetv from "./freelivetv.png";
